d6cebcb73bc5c9ba961689fb91cd335b48d81bb1
Whitewalker94 <52886389+Whitewalker94@users.noreply.github.com>
2020-03-27 22:10:05 +0100

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,12 @@
+let name = 'Ibeanu Nnamdi Emmanuel';
+let courses = ['html','css','javascript','ui/ux'];
+
+console.log(`My name is: ${name}`);
+console.log(courses);
+
+for (let i=0; i<=200; i++) {
+    if (courses.length % 2 === 0) {
+        console.log(i);
+    } 
+    
+}
